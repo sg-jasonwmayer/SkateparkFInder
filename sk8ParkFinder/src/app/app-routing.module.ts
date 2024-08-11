fd82ac344cdf5c8f49a7ec8309f0094ada9a1274
Jason Mayer <flavorflavor@gmail.com>
2024-08-11 19:27:30 -0400

    new stuff

--- sk8ParkFinder/src/app/app-routing.module.ts
+++ sk8ParkFinder/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/action',
+    pathMatch: 'full'
+  },
+  {
+    path: 'action',
+    loadChildren: () => import('./home/home.component').then((m) => m.HomeComponent)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
